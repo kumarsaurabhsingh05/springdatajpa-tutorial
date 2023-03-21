b411e75b4e0bc59f7eef3c84bd14d13e7ba90339
kumarsaurabhsingh05 <108813273+kumarsaurabhsingh05@users.noreply.github.com>
2023-03-21 15:24:38 +0530

    Spring-Data-JPA tutorial project

---
+++ src/main/java/com/dailycodebuffer/springdatajpa/springdatajpa/tutorial/SpringdatajpaTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.dailycodebuffer.springdatajpa.springdatajpa.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdatajpaTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdatajpaTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dailycodebuffer/springdatajpa/springdatajpa/tutorial/SpringdatajpaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailycodebuffer.springdatajpa.springdatajpa.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdatajpaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
